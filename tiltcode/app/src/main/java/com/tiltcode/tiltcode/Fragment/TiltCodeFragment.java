386596eb6f5aed042a7ce8fa788833891182237b
pbes0707 <pbes0707@naver.com>
2015-07-04 21:51:10 +0900

    Tilt Code Fragment add Accel

--- tiltcode/app/src/main/java/com/tiltcode/tiltcode/Fragment/TiltCodeFragment.java
+++ tiltcode/app/src/main/java/com/tiltcode/tiltcode/Fragment/TiltCodeFragment.java
@@ -1,14 +1,21 @@
 package com.tiltcode.tiltcode.Fragment;
 
+import android.app.ActivityManager;
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.tiltcode.tiltcode.Adapter.MainPagerAdapter;
+import com.tiltcode.tiltcode.Model.AccelData;
 import com.tiltcode.tiltcode.R;
 import com.tiltcode.tiltcode.View.BackFragment;
 
@@ -17,8 +24,13 @@ import com.tiltcode.tiltcode.View.BackFragment;
  * Created by JSpiner on 2015. 6. 15..
  * Contact : jspiner@naver.com
  */
-public class TiltCodeFragment extends BackFragment {
+public class TiltCodeFragment extends BackFragment implements SensorEventListener {
 
+
+    private AccelData accelData = null;
+    private ActivityManager mActivityManager;
+    private SensorManager mSensorManager;
+    private Sensor accelerometerSensor;
     //로그에 쓰일 tag
     public static final String TAG = TiltCodeFragment.class.getSimpleName();
 
@@ -32,20 +44,57 @@ public class TiltCodeFragment extends BackFragment {
     }
 
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = null;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(layoutid, container, false);
+        init();
 
-        if (v == null) {
-//            v = inflater.inflate(layoutid, null);
-            v = inflater.inflate(layoutid, container, false);
-            init();
-        }
+        mActivityManager = (ActivityManager) getActivity().getSystemService(Context.ACTIVITY_SERVICE);
+        mSensorManager = (SensorManager)getActivity().getSystemService(getActivity().SENSOR_SERVICE);
+        accelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mSensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
         return v;
     }
 
     void init() {
+    }
+
+    @Override
+    public void onDestroy() {
+        mSensorManager.unregisterListener(this);
+        super.onDestroy();
+    }
+    @Override
+    public void onResume()
+    {
+        accelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
 
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        switch(event.sensor.getType())
+        {
+            // 가속도 센서일때
+            case Sensor.TYPE_ACCELEROMETER:
+            {
+                float[] values = event.values;
+                accelData = new AccelData(
+                        Math.round(values[0] * 100d) / 100d,
+                        Math.round(values[1] * 100d) / 100d,
+                        Math.round(values[2] * 100d) / 100d);
+                break;
+            }
+        }
     }
 
 
