603ac47d08ad137d42436951cc63a0b54cd7fafb
JSpiner <jspiner@naver.com>
2015-07-10 20:40:35 +0900

    unfoldablelayout fixed and listview add

--- TiltCodeManager/app/src/main/java/com/tiltcode/tiltcodemanager/Fragment/CouponListFragment.java
+++ TiltCodeManager/app/src/main/java/com/tiltcode/tiltcodemanager/Fragment/CouponListFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -52,6 +53,13 @@ public class CouponListFragment extends Fragment {
     ListView listView;
     CouponListAdapter adapter ;
 
+    public static View.OnTouchListener interceptTouch = new View.OnTouchListener(){
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            return false;
+        }
+    };
 
     View mListTouchInterceptor;
     UnfoldableView mUnfoldableView;
@@ -72,6 +80,14 @@ public class CouponListFragment extends Fragment {
             v = inflater.inflate(layoutid, null);
 
             listView = (ListView)v.findViewById(R.id.lv_couponlist);
+
+            mListTouchInterceptor = v.findViewById(R.id.touch_interceptor_view);
+            mUnfoldableView = (UnfoldableView) v.findViewById(R.id.unfoldable_view);
+            mDetailsLayout = (FrameLayout)v.findViewById(R.id.details_layout);
+
+            detailView = inflater.inflate(R.layout.item_foldable_detail,null);
+            detailScroll = (ScrollView)detailView.findViewById(R.id.sv_foldable_detail);
+            mDetailsLayout.addView(detailView);
 
             init();
 
@@ -132,5 +148,102 @@ public class CouponListFragment extends Fragment {
                     }
                 });
 
+
+
+        mListTouchInterceptor.setClickable(false);
+
+        mDetailsLayout.setVisibility(View.INVISIBLE);
+
+        mUnfoldableView.setOnFoldingListener(new UnfoldableView.SimpleFoldingListener() {
+            @Override
+            public void onUnfolding(UnfoldableView unfoldableView) {
+                mListTouchInterceptor.setClickable(true);
+                mDetailsLayout.setVisibility(View.VISIBLE);
+                Log.d(TAG,"unfolding");
+                ((CouponListActivity)context).setEnableBack(true);
+            }
+
+            @Override
+            public void onUnfolded(UnfoldableView unfoldableView) {
+                Log.d(TAG,"unfolded");
+                mListTouchInterceptor.setClickable(false);
+//                ((DisableViewPager)MainActivity.mPager).enableTouch=false;
+            }
+
+            @Override
+            public void onFoldingBack(UnfoldableView unfoldableView) {
+                mListTouchInterceptor.setClickable(true);
+                Log.d(TAG,"foldingback");
+                ((CouponListActivity)context).setEnableBack(false);
+            }
+
+            @Override
+            public void onFoldedBack(UnfoldableView unfoldableView) {
+                Log.d(TAG,"foldedback");
+                mListTouchInterceptor.setClickable(false);
+                mDetailsLayout.setVisibility(View.INVISIBLE);
+//                ((DisableViewPager)MainActivity.mPager).enableTouch=true;
+            }
+
+            @Override
+            public void onFoldProgress(UnfoldableView unfoldableView, float progress) {
+                super.onFoldProgress(unfoldableView, progress);
+//                Log.d(TAG,"progress : "+progress + " / height : "+(int) ((float)layout_main_tab_height * progress) + "rheight : "+MainActivity.layout_main_tab.getHeight());
+
+//                MainActivity.layout_main_tab.animate().translationY(progress*layout_main_tab_height).start();
+//                MainActivity.layout_main_tab.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, (int) ((float)layout_main_tab_height * (1-progress))));
+            }
+        });
+
+
+        detailScroll.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+//                interceptTouch.onTouch(view,motionEvent);
+
+                touchList.add((int)motionEvent.getY());
+                if(touchList.size()>5){
+                    touchList.remove(0);
+                }
+                boolean sw = false;
+                if(touchList.size()==5)
+                    for(int i=1;i<5;i++){
+                        if(touchList.get(i)>touchList.get(0)){
+                            sw = true;
+                            break;
+                        }
+                    }
+                else{
+                    sw=true;
+                }
+                Log.d(TAG,"y : "+detailScroll.getScrollY() + "touchY : "+motionEvent.getY()+" sw : "+sw);
+
+
+                if(detailScroll.getScrollY()<10 && sw){
+                    if(!lastPressed){
+                        motionEvent.setAction(MotionEvent.ACTION_DOWN);
+                        Log.d(TAG,"send press down");
+                    }
+                    interceptTouch.onTouch(view, motionEvent);
+
+                    lastPressed = sw;
+                    return true;
+                }
+                lastPressed = sw;
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    lastPressed = false;
+                    touchList.clear();
+                }
+
+//                Log.d(TAG,"y : "+detailScroll.getScrollY());
+//                Log.d(TAG,"y : "+detailScroll.getScrollY());
+                return false;
+            }
+        });
+
     }
+
+
+    boolean lastPressed;
+    ArrayList<Integer> touchList = new ArrayList<>();
 }
